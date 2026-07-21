116699e9271458ffcc02b1b2c54948132f9db835
ClementineLavender <natalibulavina78@gmail.com>
2026-07-21 12:32:16 +0200

    Initial commit

---
+++ src/main.tsx
@@ -0,0 +1,8 @@
+import App from "./components/App/App";
+import { createRoot } from "react-dom/client";
+
+createRoot(document.getElementById("root") as HTMLElement).render(
+  <div>
+    <App />
+  </div>,
+);
